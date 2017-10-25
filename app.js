e8b9a5b4c57c0b324becdd706482c4d7833b1233
David Schwarzmann <dev@dschwarz.codes>
2017-10-25 15:39:19 +0200

    [ADD] Add minimal chat protocol

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const WebSocketServer = require('websocket').server;
 const http = require('http');
 
-const port = process.env['PORT']|| process.env['APP_PORT'] || 5000;
+const port = process.env['PORT'] || process.env['APP_PORT'] || 5000;
 
-const server = http.createServer(function(request, response) {
-  console.log((new Date()) + ' Received request for ' + request.url);
+const server = http.createServer(function (request, response) {
+  console.log((new Date()) + ' | Received request for ' + request.url);
   response.writeHead(404);
   response.end();
 });
 
-server.listen(port, function() {
-  console.log((new Date()) + ' Server is listening on port ' + port);
+server.listen(port, function () {
+  console.log((new Date()) + ' | Server is listening on port ' + port);
 });
 
 wsServer = new WebSocketServer({
@@ -27,29 +27,73 @@ function originIsAllowed(origin) {
   return true;
 }
 
-wsServer.on('request', function(request) {
+
+function handleJoin(msg, cb) {
+  if(!msg.hasOwnProperty('nickname') && !msg.hasOwnProperty('sent')) {
+    msg.connection.sendUTF(JSON.stringify({status: 400, message: 'Missing argument'}));
+  } else {
+    msg.connection.sendUTF(JSON.stringify({
+      type: msg.type,
+      success: true,
+      nickname: msg.nickname,
+      sent: msg.sent})
+    );
+  }
+}
+
+function handleCM(msg, cb) {
+  if(!msg.hasOwnProperty('text')) {
+    msg.connection.sendUTF(JSON.stringify({status: 400, message: 'Missing argument'}));
+  } else {
+    msg.connection.sendUTF(JSON.stringify({
+      type: msg.type,
+      message: msg.text,
+      nickname: msg.nickname,
+      sent: msg.sent})
+    );
+  }
+}
+
+wsServer.on('request', function (request) {
   if (!originIsAllowed(request.origin)) {
     // Make sure we only accept requests from an allowed origin
     request.reject();
-    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    console.log((new Date()) + ' | Connection from origin ' + request.origin + ' rejected.');
     return;
   }
 
   let connection = request.accept('hsc-protocol', request.origin);
   console.log((new Date()) + ' Connection accepted.');
 
-  connection.on('message', function(message) {
+  connection.on('message', function (message) {
+
     if (message.type === 'utf8') {
       console.log('Received Message: ' + message.utf8Data);
-      connection.sendUTF(message.utf8Data);
-    }
-    else if (message.type === 'binary') {
-      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-      //connection.sendBytes(message.binaryData);
+
+      let data = JSON.parse(message.utf8Data);
+
+      // Check if message type is available
+      if(!data.hasOwnProperty('type')) {
+        connection.sendUTF(JSON.stringify({status: 400, message: 'Not so okayish'}));
+      }
+
+      data.connection = connection;
+
+      // Check
+      switch(data.type) {
+        case 'join':
+         handleJoin(data);
+         break;
+        case 'message':
+          handleCM(data);
+          break;
+        default:
+          connection.sendUTF(message.utf8Data);
+      }
     }
   });
 
-  connection.on('close', function(reasonCode, description) {
-    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  connection.on('close', function (reasonCode, description) {
+    console.log((new Date()) + ' | Peer ' + connection.remoteAddress + ' disconnected with code ' + reasonCode + ' - ' + description);
   });
 });
