ecf497a3428ecf5eef439443ae05a474ffee7a41
ZeroSoulEater <t1@purzner.de>
2018-05-12 17:28:26 +0200

    [ADD] restriction to admin and moderator

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -10,6 +10,7 @@ module.exports = function(app) {
       type: DataTypes.UUID,
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4
+<<<<<<< master
     },
     createdAt: {
       type: DataTypes.DATE,
@@ -81,6 +82,64 @@ module.exports = function(app) {
       }
     },
   });
+=======
+		},
+		createdAt: {
+			type: DataTypes.DATE,
+			default: DataTypes.NOW
+		},
+		updatedAt: {
+			type: DataTypes.DATE,
+			allowNull: true,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		hsid: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		prename: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		lastname: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		status: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+		meter_to_hs: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		location_in_hs: {
+			type: DataTypes.BOOLEAN,
+			allowNull: true,
+		},
+		location_check_time: {
+			type: DataTypes.DATE,
+			allowNull: true,
+		},
+    role: {
+      type: DataTypes.STRING,
+      default: 'default',
+    }
+	}, {
+		hooks: {
+			beforeCount(options) {
+				options.raw = true;
+			}
+		}
+	});
+>>>>>>> [ADD] restriction to admin and moderator
 
   users.associate = function(models) { // eslint-disable-line no-unused-vars
     // Define associations here
