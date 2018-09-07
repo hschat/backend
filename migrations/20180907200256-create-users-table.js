42a63ef9e94b3d59b726cb1f3d110761e298f44d
David <dev@dschwarz.codes>
2018-09-07 22:13:14 +0200

    [FIX] Fix migrations error due to incomplete database migrations

---
+++ migrations/20180907200256-create-users-table.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: (queryInterface, DataTypes) =>
+    queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        default: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hsid: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      prename: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      meter_to_hs: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      location_in_hs: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
+      location_check_time: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      role: {
+        type: DataTypes.STRING,
+        default: 'default',
+      },
+      isVerified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      verifyToken: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      verifyExpires: {
+        type: DataTypes.DATE,
+      },
+      resetToken: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    }),
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  },
+};
---
+++ migrations/20180907200922-create-messages-table.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: (queryInterface, DataTypes) =>
+    queryInterface.createTable('chats', {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        default: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      chat_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        default: DataTypes.UUIDV4,
+      },
+      sender_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        default: DataTypes.UUIDV4,
+      },
+      send_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        default: DataTypes.NOW,
+      },
+      recieve_date: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      system: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      text: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    }),
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  },
+};
