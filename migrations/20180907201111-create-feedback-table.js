9dbf96577e95e603a47527e8b9244b5fce75fd42
Christian <Totenfluch@users.noreply.github.com>
2018-10-19 21:59:14 +0200

    Lint fixes (#14)
    * Fix test users.test to make project stable
    
    * Fixes the user.test service test
    
    * make mocha end after successfull tests
    
    * fixed linter errors, required lint to build
    
    * activated eslint on all useful directories

--- migrations/20180907201111-create-feedback-table.js
+++ migrations/20180907201111-create-feedback-table.js
@@ -1,33 +1,32 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
-  up: (queryInterface, DataTypes) =>
-    queryInterface.createTable('feedback', {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-      },
-      text: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      important: {
-        type: DataTypes.BOOLEAN,
-        allowNull: true,
-      },
-      tags: {
-        type: DataTypes.ARRAY(DataTypes.STRING),
-        defaultValue: [],
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        default: DataTypes.NOW,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-    }),
+  up: (queryInterface, DataTypes) => queryInterface.createTable('feedback', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    tags: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      defaultValue: [],
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      default: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  }),
 
   down: (queryInterface, Sequelize) => {
     /*
--- migrations/seeders/20180716100333-admin-user.js
+++ migrations/seeders/20180716100333-admin-user.js
@@ -1,22 +1,16 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-
-    return queryInterface.bulkInsert('users', [{
-        id: '883a5036-00b5-48b9-88f1-2d6d41387d42',
-        prename: 'Admin',
-        lastname: 'User',
-        email: 'admin@hschat.app',
-        password: '$2a$12$iUNtSmr4TY/n/HJFgrl8EuwJPqIIcQyK42nkmmO6u.A.Xj2oif4E2',
-        isVerified: true,
-        verifyToken: 123,
-        resetToken: 123,
-        role: 'admin',
-        hsid: 'admin',
-        createdAt: '2018-05-04 12:34:03.692+02',
-        updatedAt: '2018-05-04 12:34:03.692+02'
-      }], {});
-  }
-
+  up: queryInterface => queryInterface.bulkInsert('users', [{
+    id: '883a5036-00b5-48b9-88f1-2d6d41387d42',
+    prename: 'Admin',
+    lastname: 'User',
+    email: 'admin@hschat.app',
+    password: '$2a$12$iUNtSmr4TY/n/HJFgrl8EuwJPqIIcQyK42nkmmO6u.A.Xj2oif4E2',
+    isVerified: true,
+    verifyToken: 123,
+    resetToken: 123,
+    role: 'admin',
+    hsid: 'admin',
+    createdAt: '2018-05-04 12:34:03.692+02',
+    updatedAt: '2018-05-04 12:34:03.692+02',
+  }], {}),
 };
