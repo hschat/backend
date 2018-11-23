75e93400d14a3f1157bb395533cbb54200086a87
Lukas Hoellein <lukashoellein@googlemail.com>
2018-11-23 17:36:41 +0100

    write tests

--- test/services/chats.test.js
+++ test/services/chats.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const faker = require('faker');
 const app = require('../../src/app');
 
 describe('\'chats\' service', () => {
@@ -6,5 +7,25 @@ describe('\'chats\' service', () => {
     const service = app.service('chats');
 
     assert.ok(service, 'Registered the service');
+  });
+});
+
+describe('group chat with admins creation', () => {
+  it('should succeed with valid group chat with admins creation', () => {
+    const service = app.service('chats');
+
+    const myAdmins = [];
+    myAdmins.push(faker.fake('{{random.uuid}}'));
+    myAdmins.push(faker.fake('{{random.uuid}}'));
+
+    service.create({
+      type: 'group',
+      name: 'test-group-with-admins',
+      participants: myAdmins,
+      admins: myAdmins,
+    });
+
+    assert.ok(service);
+    assert.ok(true, 'Error occured in the group chat with admins creation');
   });
 });
