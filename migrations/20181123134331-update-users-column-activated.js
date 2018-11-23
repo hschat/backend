fc900eac850c5899c01b98dd4746dce99bda1733
owlilicious <Oliver.Korn@stud.hs-coburg.de>
2018-11-23 15:31:47 +0100

    Added activated column for user services (#32)

---
+++ migrations/20181123134331-update-users-column-activated.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    queryInterface.addColumn(
+      'users',
+      'is_activated', {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+      },
+    );
+  },
+
+  down: (queryInterface) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    queryInterface.removeColumn('users', 'is_activated');
+  },
+};
