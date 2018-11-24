8506252311f4cb68932d81f8a33a3a98d74bd6bd
Oliver Korn <Oliver.Korn@stud.hs-coburg.de>
2018-11-24 19:04:40 +0100

    Forgot to stage this change

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -90,6 +90,15 @@ module.exports = (app) => {
         type: DataTypes.BOOLEAN,
         allowNull: true,
       },
+      is_activated: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      deactivated_since: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
     },
     {
       hooks: {
