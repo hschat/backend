a1f535064e27fc21d7a40c408200e97e3dba8168
Oliver Korn <Oliver.Korn@stud.hs-coburg.de>
2018-11-27 00:03:45 +0100

    Added new Tests messages and chats +combined CleanFiles for local DB

--- test/services/messages.test.js
+++ test/services/messages.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const faker = require('faker');
 const app = require('../../src/app');
 
 describe('\'messages\' service', () => {
@@ -6,5 +7,39 @@ describe('\'messages\' service', () => {
     const service = app.service('messages');
 
     assert.ok(service, 'Registered the service');
+  });
+});
+describe('messages service create and find', () => {
+  it('create a new message', async () => {
+    const helpUser = await app.service('users').create({
+      prename: 'TestTest',
+      lastname: faker.name.lastName(),
+      email: faker.internet.email(),
+      hsid: 'test1234',
+      password: faker.internet.password(),
+    });
+
+    const service = app.service('messages');
+
+    const message = await service.create({
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      chat_id: faker.random.uuid(),
+      sender_id: helpUser.id,
+      send_date: Date.now(),
+      recieve_date: Date.now(),
+      system: true,
+      text: 'This is a test',
+    });
+
+    const foundMessages = await app.service('messages').find({
+      query: {
+        $limit: 1,
+        text: 'This is a test',
+      },
+    });
+    assert.ok(message !== undefined, 'Error at creating a new message');
+    assert.ok(message.text === 'This is a test');
+    assert.ok(foundMessages.length !== 0);
   });
 });
